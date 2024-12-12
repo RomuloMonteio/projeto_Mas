db7257b1ebb6293972ed99507a0aeb2af48cebf8
Romulo Monteiro <148643335+RomuloMonteio@users.noreply.github.com>
2024-12-12 22:00:27 +0000

    dia 12

---
+++ paciente.js
@@ -0,0 +1,53 @@
+      // Knockout ViewModel
+      var vm = function() {
+        this.nome = ko.observable("Michele Morrone");
+        this.idade = ko.observable("34 Anos");
+        this.nu = ko.observable("137943");
+        this.medico = ko.observable("Kendrick Lamar Lionel");
+        this.hospital = ko.observable("Hospital da Luz");
+
+        this.editar = () => {
+            $("input").prop("readonly", false);
+        };
+    }
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const overlay = document.getElementById("overlay");
+        const closeOverlayButton = document.getElementById("voltar");
+        const overlay2 = document.getElementById("overlay2");
+        const closeOverlayButton2 = document.getElementById("voltar2");
+        const overlay3 = document.getElementById("overlay3");
+        const closeOverlayButton3 = document.getElementById("voltar3");
+
+        // Mostrar o overlay
+        document.getElementById('Grafico').addEventListener("click", function () {
+            overlay.classList.remove("d-none");
+        });
+        document.getElementById('Grafico2').addEventListener("click", function () {
+            overlay2.classList.remove("d-none");
+        });
+        document.getElementById('Grafico3').addEventListener("click", function () {
+            overlay3.classList.remove("d-none");
+        });
+
+        // Ocultar o overlay
+        closeOverlayButton.addEventListener("click", function () {
+            overlay.classList.add("d-none");
+        });
+        closeOverlayButton2.addEventListener("click", function () {
+            overlay2.classList.add("d-none");
+        });
+        closeOverlayButton3.addEventListener("click", function () {
+            overlay3.classList.add("d-none");
+        });
+    });
+
+    // Apply Knockout bindings
+    $(document).ready(function () {
+        console.log("ready!");
+        VM = new vm() 
+        ko.applyBindings(VM);
+    });
+
+    
+    
